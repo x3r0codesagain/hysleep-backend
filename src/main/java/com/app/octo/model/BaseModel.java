7d9a859886ef70dec43a7ff48a2327d2d13e52bd
Nico Immanuel <=>
2025-02-17 14:47:37 +0700

    base model

--- src/main/java/com/app/octo/model/BaseModel.java
+++ src/main/java/com/app/octo/model/BaseModel.java
@@ -1,20 +1,25 @@
 package com.app.octo.model;
 
-import java.time.Instant;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import lombok.Data;
+import java.time.LocalDateTime;
 
-@Data
-public class BaseModel {
-    
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) 
+public abstract class BaseModel {
+
     @CreatedDate
-    private Instant createdAt;
-    
-    @LastModifiedDate
-    private Instant updatedAt;
+    @Column(name = "created_at", updatable = false, nullable = false)  
+    private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
 }
