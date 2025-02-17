d5d213376ec14b3ef98836f14d0167aba16107f6
Nico Immanuel <=>
2025-02-17 11:36:06 +0700

    room and booking entities

--- src/main/java/com/app/octo/model/response/ItemResponse.java
+++ src/main/java/com/app/octo/model/response/ItemResponse.java
@@ -2,9 +2,11 @@ package com.app.octo.model.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
 @AllArgsConstructor
+@Data
 public class ItemResponse {
   private String sku;
   private String name;
