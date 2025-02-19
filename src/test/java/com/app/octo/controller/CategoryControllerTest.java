1d792deb48082a2ea5b7a976bb25bd358dc31917
luckylaurens15 <lucky.laurens@binus.ac.id>
2025-02-19 09:56:42 +0700

    change tearDown into public

--- src/test/java/com/app/octo/controller/CategoryControllerTest.java
+++ src/test/java/com/app/octo/controller/CategoryControllerTest.java
@@ -241,7 +241,7 @@ public class CategoryControllerTest {
         categoryGetResponse.setData(new ArrayList<>());    
     }
     @AfterEach
-    private void tearDown() {
+    public void tearDown() {
         verifyNoMoreInteractions(categoryService);
     }
 }
